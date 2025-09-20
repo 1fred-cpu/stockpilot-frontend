8bbffb834c5cc2a9672fa3ba8ce044740c84569a
frederick <frederickfrimpong284@gmail.com>
2025-09-19 23:37:14 -0700

    feat: Enhance SaleKPI component to display dynamic revenue and sales data with currency symbol

--- src/components/SaleKPI.tsx
+++ src/components/SaleKPI.tsx
@@ -7,7 +7,7 @@ import axiosInstance from "../../utils/axiosInstance";
 import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "./ui/skeleton";
 import ErrorScreen from "./ErrorScreen";
-
+import { getCurrencySymbol } from "../../utils/currency";
 export default function SaleKPI() {
   const { getActiveStore } = useStore();
   const store = getActiveStore();
@@ -50,36 +50,83 @@ export default function SaleKPI() {
           <DollarSign className="h-5 w-5 dark:text-green-300 text-green-600" />
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold text-foreground">$87,456</div>
-          <p className="text-xs dark:text-green-300 text-green-600 flex items-center mt-1.5">
-            <TrendingUp className="w-4 h-4 mr-1" />
-            +15.3% from last month
+          <div className="text-2xl font-bold text-foreground">
+            {getCurrencySymbol(store?.currency as string)}
+            {data?.revenue?.total}
+          </div>
+          <p className="text-xs dark:text-green-300 text-green-600 flex items-center mt-1.5 gap-1">
+            {data?.revenue?.change !== undefined && (
+              <TrendingUp
+                className={`w-4 h-4 mr-1 ${
+                  data.revenue.change > 0
+                    ? "text-green-600 dark:text-green-300"
+                    : data.revenue.change < 0
+                    ? "rotate-180 text-red-600 dark:text-red-300"
+                    : "text-gray-400"
+                }`}
+              />
+            )}
+            {data?.revenue?.change !== undefined && (
+              <>
+                {data.revenue.change > 0 ? "+" : ""}
+                {data.revenue.change}
+              </>
+            )}{" "}
+            from last month
           </p>
         </CardContent>
       </Card>
 
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle>Total Orders</CardTitle>
+          <CardTitle>Total Sales</CardTitle>
           <ShoppingCart className="h-5 w-5 dark:text-blue-300 text-blue-600" />
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold text-foreground mb-1">1,247</div>
-          <p className="text-xs dark:text-blue-300 text-blue-600">
-            +89 orders this week
+          <div className="text-2xl font-bold text-foreground mb-1">
+            {data?.sales?.total}
+          </div>
+          <p className="text-xs dark:text-blue-300 text-blue-600 flex items-center mt-1.5">
+            {data?.sales?.change !== undefined && (
+              <>
+                {data.sales.change > 0 ? "+" : ""}
+                {data.sales.change}
+              </>
+            )}{" "}
+            sales this week
           </p>
         </CardContent>
       </Card>
 
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle>Average Order Value</CardTitle>
+          <CardTitle>Average sale Value</CardTitle>
           <TrendingUp className="h-5 w-5 text-purple-600 dark:text-purple-300" />
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold text-foreground mb-1">$70.15</div>
-          <p className="text-xs text-purple-600 dark:text-purple-300">
-            +5.2% from last week
+          <div className="text-2xl font-bold text-foreground mb-1">
+            {getCurrencySymbol(store?.currency as string)}
+            {data?.averageSaleValue?.value}
+          </div>
+          <p className="text-xs text-purple-600 dark:text-purple-300 mt-1.5 flex items-center gap-1">
+            {data?.averageSaleValue?.change !== undefined && (
+              <TrendingUp
+                className={`w-4 h-4 mr-1 ${
+                  data.averageSaleValue.change > 0
+                    ? "text-green-600 dark:text-green-300"
+                    : data.averageSaleValue.change < 0
+                    ? "rotate-180 text-red-600 dark:text-red-300"
+                    : "text-gray-400"
+                }`}
+              />
+            )}
+            {data?.averageSaleValue?.change !== undefined && (
+              <>
+                {data.averageSaleValue.change > 0 ? "+" : ""}
+                {data.averageSaleValue.change}
+              </>
+            )}{" "}
+            from last week
           </p>
         </CardContent>
       </Card>
